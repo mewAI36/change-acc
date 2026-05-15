bc7b30f29a0511953a9dd5324f009a6fde70867e
mewAI36 <tranphuchung4069@gmail.com>
2026-05-15 20:51:08 +0700

    Add files via upload

--- aotr.lua
+++ aotr.lua
@@ -1,74 +1,12 @@
-repeat task.wait() until game:IsLoaded()
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.9) ~  Much Love, Ferib 
 
-local Players = game:GetService("Players")
-local Player = Players.LocalPlayer
+]]--
 
-local Config = getgenv().Config or {
-    ["Required Gems"] = 1000000,
-    ["Check Delay"] = 60
-}
-
-local RequiredGems = tonumber(Config["Required Gems"])
-
-local function getGems()
-
-    local success, result = pcall(function()
-
-        local amountLabel = Player
-            :WaitForChild("PlayerGui")
-            :WaitForChild("Interface")
-            :WaitForChild("Topbar")
-            :WaitForChild("Main")
-            :WaitForChild("Currencies")
-            :WaitForChild("Gems")
-            :WaitForChild("Amount")
-
-        local text = amountLabel.Text:gsub(",", "")
-
-        return tonumber(text)
-    end)
-
-    if success then
-        return result
-    end
-
-    warn(result)
-    return nil
-end
-
-local function checkGems()
-
-    local gems = getGems()
-
-    if gems then
-
-        print("Current Gems:", gems)
-
-        if gems >= RequiredGems then
-
-            writefile(
-                Player.Name .. ".txt",
-                "Completed-AOTR_" .. tostring(gems)
-            )
-
-            print("Saved:", Player.Name .. ".txt")
-
-            return true
-        end
-    end
-
-    return false
-end
-
--- check ngay khi exec
-if checkGems() then
-    return
-end
-
--- sau đó mỗi 60s
-while task.wait(Config["Check Delay"]) do
-
-    if checkGems() then
-        break
-    end
-end
+repeat task.wait();until game:IsLoaded() local v0=game:GetService("Players");local v1=v0.LocalPlayer;local v2=getgenv().Config or {["Required Gems"]=1000877 -(282 + 595) ,["Check Delay"]=60} ;local v3=tonumber(v2["Required Gems"]);local function v4() local v6,v7=pcall(function() local v10=v1:WaitForChild("PlayerGui"):WaitForChild("Interface"):WaitForChild("Topbar"):WaitForChild("Main"):WaitForChild("Currencies"):WaitForChild("Gems"):WaitForChild("Amount");local v11=v10.Text:gsub(",","");return tonumber(v11);end);if v6 then return v7;end warn(v7);return nil;end local function v5() local v8=1637 -(1523 + 114) ;local v9;while true do if (v8==(1 + 0)) then return false;end if (v8==(0 -0)) then v9=v4();if v9 then print("Current Gems:",v9);if (v9>=v3) then local v12=568 -(367 + 201) ;while true do if (v12==1) then return true;end if (v12==(927 -(214 + 713))) then writefile(v1.Name   .. ".txt" ,"Completed-AOTR_"   .. tostring(v9) );print("Saved:",v1.Name   .. ".txt" );v12=1 + 0 ;end end end end v8=1 + 0 ;end end end if v5() then return;end while task.wait(v2["Check Delay"]) do if v5() then break;end end
